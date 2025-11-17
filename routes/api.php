8c4a3cdb0128d1047544e2e0414c88e1a839211a
Mr.Professor <mronx@ya.ru>
2025-11-17 14:15:57 +0500

    first commit

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+
+Route::prefix('v1')->group(base_path('routes/v1/index.php'));
